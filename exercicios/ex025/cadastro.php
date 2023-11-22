b17ddc8bbe0c5de20668deba6642db2d2728eace
Erik Vieira <89081027+erik-vieira@users.noreply.github.com>
2023-11-22 08:59:26 -0300

    forms

--- exercicios/ex025/cadastro.php
+++ exercicios/ex025/cadastro.php
@@ -1,2 +1,14 @@
-Os dados foram enviados!
-Aprenda PHP para saber o que fazer com eles.
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <h1>Cadastro realizado com sucesso!!</h1>
+    <?php
+    echo "Cadastro realizado com sucesso"
+        ?>
+</body>
+</html>
